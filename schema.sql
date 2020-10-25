ea7d6cb23908670d0ce270b7947a8061d91a721a
Joseph Torres <simply.joseph00@gmail.com>
2020-10-25 02:03:29 -0400

    added sql files

---
+++ schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS employee_manager
+CREATE DATABASE employee_manager
+USE employee_manager
+CREATE TABLE role
+(
+    id INT
+    AUTO_INCREMENT,
+    title VARCHAR
+    (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    manager_key INT NUll,
+    PRIMARY KEY
+    (id)
+);
+    CREATE TABLE employee
+    (
+        id INT
+        AUTO_INCREMENT,
+    first_name VARCHAR
+        (30) NOT NULL,
+    last_name VARCHAR
+        (30) NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY
+        (id)
+);
+        CREATE TABLE department
+        (
+            id INT
+            AUTO_INCREMENT,
+    department_name VARCHAR
+            (30) NOT NULL,
+    PRIMARY KEY
+            (id)
+);
